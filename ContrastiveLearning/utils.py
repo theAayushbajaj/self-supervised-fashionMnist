2ac98944958c8a357f61fac834795f4a0d728ac0
Aayush Bajaj <microbot@aayushs-air.home>
2024-02-19 18:51:08 -0500

    added simclr encoded represtations to classifier

--- ContrastiveLearning/utils.py
+++ ContrastiveLearning/utils.py
@@ -7,7 +7,6 @@ from torch.utils.data.dataset import Subset
 from sklearn.model_selection import train_test_split
 
 import torch
-import yaml
 
 class GaussianBlur(object):
     """blur a single image on CPU"""
@@ -56,11 +55,11 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
         shutil.copyfile(filename, 'model_best.pth.tar')
 
 
-def save_config_file(model_checkpoints_folder, args):
-    if not os.path.exists(model_checkpoints_folder):
-        os.makedirs(model_checkpoints_folder)
-        with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
-            yaml.dump(args, outfile, default_flow_style=False)
+# def save_config_file(model_checkpoints_folder, args):
+#     if not os.path.exists(model_checkpoints_folder):
+#         os.makedirs(model_checkpoints_folder)
+#         with open(os.path.join(model_checkpoints_folder, 'config.yml'), 'w') as outfile:
+#             yaml.dump(args, outfile, default_flow_style=False)
 
 
 def accuracy(output, target, topk=(1,)):
