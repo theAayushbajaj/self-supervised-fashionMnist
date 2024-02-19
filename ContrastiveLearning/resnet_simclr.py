880b943ad4652c9f541b385d2293e27755fdf1eb
Aayush Bajaj <microbot@aayushs-air.home>
2024-02-19 01:32:18 -0500

    added support for contrastive loss

--- ContrastiveLearning/resnet_simclr.py
+++ ContrastiveLearning/resnet_simclr.py
@@ -5,9 +5,9 @@ class ProjectionHead(nn.Module):
     def __init__(self, in_dim=128, out_dim=128):
         super(ProjectionHead, self).__init__()
         self.layers = nn.Sequential(
-            nn.Linear(in_dim, 128),
+            nn.Linear(in_dim, 512),
             nn.ReLU(),
-            nn.Linear(128, out_dim),
+            nn.Linear(512, out_dim),
         )
         
     def forward(self, x):
@@ -17,10 +17,12 @@ class ProjectionHead(nn.Module):
 class ResNetSimCLR(nn.Module):
     def __init__(self, out_dim=128):
         super(ResNetSimCLR, self).__init__()
-        self.resnet = models.resnet18(pretrained=False)
+        self.resnet = models.resnet18(weights=None)
+
+        dim_mlp = self.resnet.fc.in_features
         self.resnet.fc = nn.Identity()
 
-        self.projection_head = ProjectionHead(in_dim=self.resnet.fc.in_features, out_dim=out_dim)
+        self.projection_head = ProjectionHead(in_dim=dim_mlp, out_dim=out_dim)
 
     def forward(self, x_i, x_j):
         h_i = self.resnet(x_i)
