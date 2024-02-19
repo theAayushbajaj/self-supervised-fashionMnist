5dc3da69e8135476ca443ca6db60e12292e74d83
Aayush Bajaj <microbot@aayushs-air.home>
2024-02-19 02:27:05 -0500

    minor changes

--- ContrastiveLearning/ntxent_loss.py
+++ ContrastiveLearning/ntxent_loss.py
@@ -11,11 +11,11 @@ class NTXentLoss(nn.Module):
         self.criterion = nn.CrossEntropyLoss(reduction="sum")
 
     def forward(self, z_i, z_j):
-        N = 2 * self.batch_size
+        N = self.batch_size
         z = torch.cat((z_i, z_j), dim=0)
 
         z_norm = F.normalize(z, p=2, dim=1)
-        sim_matrix = torch.mm(z_norm, z_norm.transpose(0, 1)) / self.temperature
+        sim_matrix = torch.mm(z_norm, z_norm.transpose(0, 1))
         #sim_matrix = F.cosine_similarity(z.unsqueeze(1), z.unsqueeze(0), dim=2) / self.temperature
         sim_matrix = sim_matrix - torch.eye(N, device=self.device) * 1e9
 
@@ -28,7 +28,7 @@ class NTXentLoss(nn.Module):
 
         loss_per_sample = -torch.log(positives / negatives)
 
-        loss = torch.sum(loss_per_sample) / (2 * self.batch_size)
+        loss = torch.sum(loss_per_sample) / (self.batch_size)
         return loss
 
 
